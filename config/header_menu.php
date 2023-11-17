c6cd37bea265b2b398fee9c3c5eef0c7b99230e4
R-G2110 <argeldelacruz86@gmail.com>
2023-11-17 15:05:44 +0100

    [Add]: menu on header

---
+++ config/header_menu.php
@@ -0,0 +1,33 @@
+<?php
+
+return[
+    "genere" => [
+        [
+            "href" => '',
+		    "text" => "Donna"
+        ],
+        [
+            "href" => '',
+		    "text" => "Uomo"
+        ],
+        [
+            "href" => '',
+		    "text" => "Bambini"
+        ]
+    ],
+    'userMenu' => [
+        [
+            "href" => "#",
+            "text" => 'fa-regular fa-user'
+        ],
+        [
+            "href" => "#",
+            "text" => 'fa-regular fa-heart'
+        ],
+        [
+            "href" => "#",
+            "text" => 'fa-solid fa-bag-shopping'
+        ],
+    ]
+];
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+@php
+    $menu_genere = config('header_menu.genere');
+    $menu_user = config('header_menu.userMenu');
+@endphp
+<header>
+
+    <div class="fixed  ">
+        <div class="container flex justify-sb">
+
+            <nav>
+                <ul class="flex">
+                    @foreach ($menu_genere as $item)
+                        <li><a href="">{{ $item['text'] }}</a></li>
+                    @endforeach
+                </ul>
+            </nav>
+
+            <div class="logo">
+                <a href="#">
+                    <img src="/img/boolean-logo.png" alt="boolean-logo">
+                </a>
+            </div>
+
+            <nav>
+                <ul class="flex">
+                    @foreach ($menu_user as $item)
+                        <li><a href=""><i class="@php echo $item['text']@endphp"></i></a></li>
+                    @endforeach
+
+                </ul>
+            </nav>
+
+        </div>
+    </div>
+
+</header>
