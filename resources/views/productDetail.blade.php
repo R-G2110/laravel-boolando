b11f980029f30df549a49928db4f89fb5a28fd5a
R-G2110 <argeldelacruz86@gmail.com>
2023-11-19 21:25:30 +0100

    [Edit]: item card on productDetail.blade.php

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -3,45 +3,43 @@
 
 @section('content')
     <div class="container flex justify-center">
-        @foreach ($products as $product)
 
-            <a class="item flex " href="{{ route('productDetail', ['slug' => $product['slug']]) }}">
+        <a class="item flex " href="{{ route('productDetail', ['slug' => $product['slug']]) }}">
 
 
-                <div class="photo-item">
+            <div class="photo-item">
 
-                    <img
-                        src="/img/{{ $product['photo'] }}"
-                        alt="{{ $product['itemName'] }}"
-                    >
-                    <img
-                        class="alternative"
-                        src="/img/{{ $product['alternative'] }}"
-                        alt="{{ $product['itemName'] }}"
-                    >
-                    <span class="heart">&hearts;</span>
+                <img
+                    src="/img/{{ $product['photo'] }}"
+                    alt="{{ $product['itemName'] }}"
+                >
+                <img
+                    class="alternative"
+                    src="/img/{{ $product['alternative'] }}"
+                    alt="{{ $product['itemName'] }}"
+                >
+                <span class="heart">&hearts;</span>
+                <span
+                    class="{{$product['discount'] === 0 ? 'hide' :''}} discount"
+
+                >-{{ $product['discount'] }}%</span>
+                <span
+                    class="{{$product['eco'] ? '' :'hide'}} {{$product['discount'] === 0 ? 'no-discount eco' :'eco'}}"
+
+                >Sostenibilità</span>
+                <div class="item-info">
+
+                    <span class="brand">{{$product['brand']}}</span>
+                    <span class="item-name">{{$product['itemName']}}</span>
+                    <span class="c-price">{{$product['currPrice']}}&euro;</span>
                     <span
-                        class="{{$product['discount'] === 0 ? 'hide' :''}} discount"
-
-                    >-{{ $product['discount'] }}%</span>
-                    <span
-                        class="{{$product['eco'] ? '' :'hide'}} {{$product['discount'] === 0 ? 'no-discount eco' :'eco'}}"
-
-                    >Sostenibilità</span>
-                    <div class="item-info">
-
-                        <span class="brand">{{$product['brand']}}</span>
-                        <span class="item-name">{{$product['itemName']}}</span>
-                        <span class="c-price">{{$product['currPrice']}}&euro;</span>
-                        <span
-                            class="{{$product['discount'] === 0 ? 'hide' :''}}"
-                            class="o-price"
-                        >{{$product['origPrice']}}&euro;</span>
-                    </div>
-
+                        class="{{$product['discount'] === 0 ? 'hide' :''}}"
+                        class="o-price"
+                    >{{$product['origPrice']}}&euro;</span>
                 </div>
 
-            </a>
-        @endforeach
+            </div>
+
+        </a>
     </div>
 @endsection
